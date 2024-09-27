ea55d7b9a63e95d947e5b86f311903ea6958bf3d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-09-27 13:34:44 +1000

    https://github.com/mP1/walkingkooka-plugin/pull/240 PluginSelector name without space open-parens
    - https://github.com/mP1/walkingkooka-plugin/pull/240
    - PluginSelector name without space open-parens

--- src/test/java/walkingkooka/convert/provider/ConverterSelectorTest.java
+++ src/test/java/walkingkooka/convert/provider/ConverterSelectorTest.java
@@ -89,8 +89,9 @@ public final class ConverterSelectorTest implements PluginSelectorLikeTesting<Co
 
         this.checkEquals(
                 new InvalidCharacterException(
-                        text, text.indexOf(' ') + 1)
-                        .getMessage(),
+                        text,
+                        text.indexOf(' ')
+                ).getMessage(),
                 thrown.getMessage()
         );
     }
@@ -146,8 +147,8 @@ public final class ConverterSelectorTest implements PluginSelectorLikeTesting<Co
     @Test
     public void testEvaluateTextOpenParensFail() {
         this.evaluateTextFails(
-                NAME + " (",
-                "Invalid character '(' at 25 in \"super-magic-converter123 (\""
+                NAME + "(",
+                "Invalid character '(' at 24 in \"super-magic-converter123(\""
         );
     }
 
@@ -201,9 +202,14 @@ public final class ConverterSelectorTest implements PluginSelectorLikeTesting<Co
 
     @Test
     public void testEvaluateTextDoubleMissingClosingParensFail() {
+        this.checkEquals(
+                25,
+                NAME.textLength() + 1
+        );
+
         this.evaluateTextFails(
-                NAME + " (1",
-                "Invalid character '1' at 26 in \"super-magic-converter123 (1\""
+                NAME + "(1",
+                "Invalid character '1' at 25 in \"super-magic-converter123(1\""
         );
     }
 
